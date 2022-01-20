db837d8a8e3eec19e237957abba598ea7583e30f
Leo Rigotto <leorigotto@me.com>
2022-01-20 17:36:23 -0300

    app.js atualizado com state e lógica atualizada com props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import { SecaoProdutos } from './components/SecaoProdutos';
+import productList from "./data/produtos.json"
 
 class App extends React.Component {
+
+  state = {
+    produtos: productList,
+    order: "descrescente",
+}
+
   render() {
   return (
     <div className="App">
-      <SecaoProdutos></SecaoProdutos>
+      <SecaoProdutos 
+      produtos={this.state.produtos} 
+      order={this.state.order}/>
     </div>
   );
 }
